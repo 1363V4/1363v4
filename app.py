74438855c0b3792e9694059286581745d12d1ac8
1363V4 <105431087+1363V4@users.noreply.github.com>
2022-11-22 15:15:35 +0100

    fiat lux

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, flash
+from utils import check_reliable, validate_deck
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def index():
+    return render_template('index.html')
+	
+@app.route("/app_1", methods=('GET', 'POST'))
+def app_1():
+	if request.method == 'POST':
+		...
+		
+	return render_template('app_1.html')
+	
