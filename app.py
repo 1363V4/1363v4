7d959a458782323d6199636d391e4633027aac07
1363V4 <105431087+1363V4@users.noreply.github.com>
2022-11-22 15:33:06 +0100

    fuck

--- app.py
+++ app.py
@@ -9,8 +9,17 @@ def index():
 	
 @app.route("/app_1", methods=('GET', 'POST'))
 def app_1():
+	output = ""
 	if request.method == 'POST':
-		...
+		input = request.form['input']
+		answers = {
+			'Sopra': "Steria",
+			'Eva': "BSSI !",
+		}
+		try:
+			output = answers[input]
+		except:
+			output = "Bad input."
 		
-	return render_template('app_1.html')
+	return render_template('app_1.html', output=output)
 	
