405f4f8736968ba8aa2af6bb8b6a9b8bbe1a8751
1363V4 <105431087+1363V4@users.noreply.github.com>
2022-11-22 16:39:43 +0100

    fuck

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ def index():
 	
 @app.route("/app_1", methods=('GET', 'POST'))
 def app_1():
-	output = "_"
 	if request.method == 'POST':
 		input = request.form['input']
 		answers = {
@@ -20,8 +19,7 @@ def app_1():
 			output = answers[input]
 		except:
 			output = "Bad input."
-		output = "yes"
-		return render_template('app_1.html', output=output)
+		flash(output)
 		
-	return render_template('app_1.html', output=output)
+	return render_template('app_1.html')
 	
