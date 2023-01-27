94dae39fa7cbfa5d12336a5ab0d53b09ae4d76f3
1363V4 <105431087+1363V4@users.noreply.github.com>
2023-01-27 14:45:04 +0100

    dev

--- app.py
+++ app.py
@@ -3,23 +3,47 @@ from flask import Flask, render_template, request, url_for, flash
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-	return render_template('index.html')
-	
-@app.route("/app_1", methods=('GET', 'POST'))
-def app_1():
-	msg="_"
-	if request.method == 'POST':
-		input = request.form['input']
-		answers = {
-			'Sopra': "Steria",
-			'Eva': "BSSI !",
-		}
-		try:
-			msg = answers[input]
-		except:
-			msg = "Bad input."
-		
-	return render_template('app_1.html', message=msg)
-	
+    return render_template('index.html')
+
+
+@app.route("/quiz")
+def quiz():
+    return render_template('quiz.html')
+
+
+@app.route("/quiz/v1", methods=('GET', 'POST'))
+def quiz_questions():
+    if request.method == 'POST':
+        input = request.form
+        answers = {
+            "pssi": {
+                "oui": 25,
+                "non": 0
+            },
+            "know": {
+                "4": 75,
+                "3": 50,
+                "2": 25,
+                "1": 0,
+            }
+        }
+        score = 0
+        for name, value in input.items():
+            score += answers[name][value]
+        msgs = {
+            100: "Très bien",
+            75: "Bien",
+            50: "Correct",
+            25: "Pas top",
+            0: "Faut consulter",
+        }
+        msg = msgs[score]
+        return render_template(
+            'quiz_results.html',
+            score=str(score),
+            message=msg
+            )
+    return render_template('quiz_questions.html')
