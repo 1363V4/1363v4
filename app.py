e47664386bdc7e5b008e1249e7c9fa110bb49b58
1363V4 <105431087+1363V4@users.noreply.github.com>
2023-02-06 16:37:05 +0100

    rev1

--- app.py
+++ app.py
@@ -19,20 +19,46 @@ def quiz_questions():
     if request.method == 'POST':
         input = request.form
         answers = {
-            "pssi": {
-                "oui": 25,
-                "non": 0
+            "dcp1": {
+                "r11": 2,
+                "r12": 1,
+                "r13": 0,
             },
-            "know": {
-                "4": 75,
-                "3": 50,
-                "2": 25,
-                "1": 0,
-            }
+            "dcp2": {
+                "r21": 2,
+                "r22": 1,
+                "r23": 0,
+            },
+            "dcp3": {
+                "r31": 2,
+                "r32": 1,
+                "r33": 0,
+            },
+            "dcp4": {
+                "r41": 2,
+                "r42": 1,
+                "r43": 0,
+            },
+            "dcp5": {
+                "r51": 2,
+                "r52": 1,
+                "r53": 0,
+            },
+            "dcp6": {
+                "r61": 2,
+                "r62": 1,
+                "r63": 0,
+            },
+            "dcp7": {
+                "r71": 2,
+                "r72": 1,
+                "r73": 0,
+            },
         }
         score = 0
         for name, value in input.items():
             score += answers[name][value]
+        score = int(int((4*score)/14)*25)
         msgs = {
             100: "Très bien",
             75: "Bien",
