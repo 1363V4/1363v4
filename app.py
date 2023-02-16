1450c1464935c488a25d5796e3b548b0a6a9f51f
1363V4 <105431087+1363V4@users.noreply.github.com>
2023-02-16 13:25:52 +0100

    dev

--- app.py
+++ app.py
@@ -1,75 +1,58 @@
 from flask import Flask, render_template, request, url_for, flash
+from settings import scores, msgs
 
 
 app = Flask(__name__)
 
-
+# HOME
 @app.route("/")
 def index():
     return render_template('index.html')
 
+#QUIZ V1
+@app.route("/quiz/v1")
+def quiz_v1():
+    return render_template('v1/quiz.html')
 
-@app.route("/quiz")
-def quiz():
-    return render_template('quiz.html')
 
-
-@app.route("/quiz/v1", methods=('GET', 'POST'))
-def quiz_questions():
+@app.route("/quiz/v1/dcp", methods=('GET', 'POST'))
+def quiz_questions_v1():
     if request.method == 'POST':
         input = request.form
-        answers = {
-            "dcp1": {
-                "r11": 2,
-                "r12": 1,
-                "r13": 0,
-            },
-            "dcp2": {
-                "r21": 2,
-                "r22": 1,
-                "r23": 0,
-            },
-            "dcp3": {
-                "r31": 2,
-                "r32": 1,
-                "r33": 0,
-            },
-            "dcp4": {
-                "r41": 2,
-                "r42": 1,
-                "r43": 0,
-            },
-            "dcp5": {
-                "r51": 2,
-                "r52": 1,
-                "r53": 0,
-            },
-            "dcp6": {
-                "r61": 2,
-                "r62": 1,
-                "r63": 0,
-            },
-            "dcp7": {
-                "r71": 2,
-                "r72": 1,
-                "r73": 0,
-            },
-        }
+        _scores = scores['dcp']
         score = 0
         for name, value in input.items():
-            score += answers[name][value]
+            score += _scores[name][value]
         score = int(int((4*score)/14)*25)
-        msgs = {
-            100: "Très bien",
-            75: "Bien",
-            50: "Correct",
-            25: "Pas top",
-            0: "Faut consulter",
-        }
-        msg = msgs[score]
+        _msgs = msgs['dcp']
+        msg = _msgs[score]
         return render_template(
-            'quiz_results.html',
+            'v1/quiz_results.html',
             score=str(score),
             message=msg
             )
-    return render_template('quiz_questions.html')
+    return render_template('v1/quiz_questions.html')
+
+#QUIZ V2
+@app.route("/quiz/v2")
+def quiz_v2():
+    return render_template('v2/quiz.html')
+
+
+@app.route("/quiz/v2/dcp", methods=('GET', 'POST'))
+def quiz_questions_v2():
+    if request.method == 'POST':
+        input = request.form
+        _scores = scores['dcp']
+        score = 0
+        for name, value in input.items():
+            score += _scores[name][value]
+        score = int(int((4*score)/14)*25)
+        _msgs = msgs['dcp']
+        msg = _msgs[score]
+        return render_template(
+            'v2/quiz_results.html',
+            score=str(score),
+            message=msg
+            )
+    return render_template('v2/quiz_questions.html')
